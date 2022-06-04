f4b1c771fd986f2daa4b26d5f10ca9ebc398f5e5
Emilio Cortina Labra <emicorti@amazon.com>
2022-06-04 17:15:38 +0200

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Cortina Labra, Emilio on 4/6/22.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
